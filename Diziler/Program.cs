7d91180cdd28f639d4f54d7d7a8869f46dd17373
rasih cakmak <38331957+rasihcakmak@users.noreply.github.com>
2023-05-20 22:39:17 +0300

    Add files via upload

---
+++ Diziler/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace abc
+{
+  class Program
+  {
+
+    
+    static void Main(string [] args)
+    {
+    string[] renkler =new string[5];
+    string[] hayvanlar= {"Kedi","Köpek","kuş", "Hayvan"};
+    int[] dizi;
+    dizi= new int[5];
+    renkler[0]="Mavi";
+    dizi[3]=10;
+
+    Console.WriteLine(hayvanlar[1]);
+    Console.WriteLine(dizi[3]);
+    Console.WriteLine(renkler[0]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    }
+}
